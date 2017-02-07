108ca8d6f4ba8129355052fc0317d616d0c6eb64
johnram528 <johnram528@gmail.com>
2017-02-07 16:31:05 -0500

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,26 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def add_item(item)
+    line_item = self.line_items.find_by(item_id: item)
+    if line_item.blank?
+      item = Item.find_by(id: item)
+      item.line_items.build(cart: self)
+    else
+      line_item.quantity += 1
+      line_item
+    end
+  end
+
+  def total
+    total = 0
+    items.each do |item|
+      total += item.price
+    end
+    total
+  end
+
 
 end
